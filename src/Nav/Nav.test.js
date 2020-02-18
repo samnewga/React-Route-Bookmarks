7ef0b5ad62eceb79bcd3c54122f30b975e068f6a
samnewga <newgate90s@gmail.com>
2020-02-17 18:10:19 -0700

    1st

--- src/Nav/Nav.test.js
+++ src/Nav/Nav.test.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import ReactDOM from 'react-dom';
+import Nav from './Nav';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(
     <BrowserRouter>
-      <App />
-    </BrowserRouter>, div);
+      <Nav />
+    </BrowserRouter>, 
+    div);
   ReactDOM.unmountComponentAtNode(div);
 });
---
+++ src/config.js
@@ -0,0 +1,4 @@
+export default {
+  API_ENDPOINT: `https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks`,
+  API_KEY: '$2a$10$AplzUPhAm.wBm1.RAUqOp.zQlQLYA9RX3A3lKxqw8fMlZIp5dcv0y',
+}
